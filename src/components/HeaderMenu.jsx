d91e99a491143d18d2dfc4770bb3a02def595a94
PisamaTech <pisamatech@gmail.com>
2025-07-29 10:51:19 -0300

    Agregado logo de Espacio Pisama al menu lateral.

--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -6,6 +6,7 @@ import {
   SidebarMenuItem,
 } from "./ui";
 import { Link } from "react-router-dom";
+import pisamaLogo from "@/assets/EspacioPimasaLogo-300.webp";
 
 export const HeaderMenu = () => {
   return (
@@ -14,8 +15,13 @@ export const HeaderMenu = () => {
         <SidebarMenuItem>
           <SidebarMenuButton size="lg" asChild>
             <Link to="/calendario">
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                <Sofa className="size-5" />
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg text-sidebar-primary-foreground">
+                {/* <Sofa className="size-5" /> */}
+                <img
+                  src={pisamaLogo}
+                  alt="Espacio Pisama Logo"
+                  className="w-8 h-8 drop-shadow-md hover:drop-shadow-xl"
+                />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight ml-2">
                 <span className="truncate font-semibold">Espacio Pisama</span>
